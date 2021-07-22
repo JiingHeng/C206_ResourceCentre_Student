44ad9065c20984b17028f450879caa4c18f19218
Tay Hao Wen <tay_haowen@hotmail.com>
2021-07-22 14:13:44 +0800

    Commit TestRetriveChromeBook

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -112,8 +112,8 @@ public class ResourceCentreTest {
 		// from the SourceCentre
 		allChromeBook = ResourceCentre.retrieveAllChromebook(chromebookList);
 
-		testOutput = String.format("%-10s %-30s %-10s \n", "CB0011", "My Google Chromebook 1st", "Mac OS");
-		testOutput += String.format("%-10s %-30s %-10s \n", "CB0012", "SAMSUNG Chromebook 4+", "Win 10");
+		testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0011", "My Google Chromebook 1st", "Yes", "" ,"Mac OS");
+		testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n", "CB0012", "SAMSUNG Chromebook 4+", "Yes", "","Win 10");
 
 		assertEquals("Check that ViewAllChromeBook", testOutput, allChromeBook);
 	}
